4a07a5b648f6f18e9fc48df66d798476c68fc71a
Thiago C. dos Santos <104088057+Thiago-CSantos@users.noreply.github.com>
2024-06-04 21:04:48 -0300

    quase la

--- p2/index.js
+++ p2/index.js
@@ -1,80 +1,49 @@
 let app = new Vue({
-    el: "#app",
-    data: {
-        carregaFormulario: null,
+  el: "#app",
+  data: {
+    carregaFormulario: false,
+    clienteExiste: false,
+    Clientes: [],
+    Vemail: '',
+    Vnome: '',
+    Vrua: '',
+    Vbairro: '',
+    Vcidade: '',
+    Vestados: ''
+  },
+  methods: {
+    actionCadastraCliente() {
+      this.carregaFormulario = true;
     },
-    methods: {
-        actionCadastraCliente() {
-            this.carregaFormulario = `<form class="row g-3">
-                <div class="col-md-6">
-                  <label for="inputEmail4" class="form-label">Email</label>
-                  <input type="email" class="form-control" id="inputEmail4">
-                </div>
-                <div class="col-md-6">
-                  <label for="inputPassword4" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="inputPassword4">
-                </div>
-                <div class="col-12">
-                  <label for="inputAddress" class="form-label">Address</label>
-                  <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                </div>
-                <div class="col-12">
-                  <label for="inputAddress2" class="form-label">Address 2</label>
-                  <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                </div>
-                <div class="col-md-6">
-                  <label for="inputCity" class="form-label">City</label>
-                  <input type="text" class="form-control" id="inputCity">
-                </div>
-                <div class="col-md-4">
-                  <label for="inputState" class="form-label">State</label>
-                  <select id="inputState" class="form-select">
-                    <option selected>Escolha um estado...</option>
-                    <option value="AC">Acre</option>
-                    <option value="AL">Alagoas</option>
-                    <option value="AP">Amapá</option>
-                    <option value="AM">Amazonas</option>
-                    <option value="BA">Bahia</option>
-                    <option value="CE">Ceará</option>
-                    <option value="DF">Distrito Federal</option>
-                    <option value="ES">Espírito Santo</option>
-                    <option value="GO">Goiás</option>
-                    <option value="MA">Maranhão</option>
-                    <option value="MT">Mato Grosso</option>
-                    <option value="MS">Mato Grosso do Sul</option>
-                    <option value="MG">Minas Gerais</option>
-                    <option value="PA">Pará</option>
-                    <option value="PB">Paraíba</option>
-                    <option value="PR">Paraná</option>
-                    <option value="PE">Pernambuco</option>
-                    <option value="PI">Piauí</option>
-                    <option value="RJ">Rio de Janeiro</option>
-                    <option value="RN">Rio Grande do Norte</option>
-                    <option value="RS">Rio Grande do Sul</option>
-                    <option value="RO">Rondônia</option>
-                    <option value="RR">Roraima</option>
-                    <option value="SC">Santa Catarina</option>
-                    <option value="SP">São Paulo</option>
-                    <option value="SE">Sergipe</option>
-                    <option value="TO">Tocantins</option>
-                </select>
-                </div>
-                <div class="col-md-2">
-                  <label for="inputZip" class="form-label">Zip</label>
-                  <input type="text" class="form-control" id="inputZip">
-                </div>
-                <div class="col-12">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck">
-                    <label class="form-check-label" for="gridCheck">
-                      Check me out
-                    </label>
-                  </div>
-                </div>
-                <div class="col-12">
-                  <button type="submit" class="btn btn-primary">Sign in</button>
-                </div>
-              </form>`;
-        }
-    }
+    enviarFormulario() {
+      
+      console.log('Dados do formulário:', this.Vemail, this.Vnome, this.Vrua, this.Vbairro, this.Vcidade, this.Vestados);
+
+      
+      this.Clientes.push({
+        email: this.Vemail,
+        nome: this.Vnome,
+        rua: this.Vrua,
+        bairro: this.Vbairro,
+        cidade: this.Vcidade,
+        estado: this.Vestados
+      });
+
+      console.log(this.Clientes);
+
+      this.resetarFormulario();
+    },
+    resetarFormulario() {
+      
+      this.Vemail = '';
+      this.Vnome = '';
+      this.Vrua = '';
+      this.Vbairro = '';
+      this.Vcidade = '';
+      this.Vestados = '';
+
+      
+      this.carregaFormulario = false;
+    },
+  }
 });
